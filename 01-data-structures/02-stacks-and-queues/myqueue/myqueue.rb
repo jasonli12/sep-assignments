c610d644d01eb449eeeb921599c5217b9c45f3c8
Jason Li <jason.li0102@gmail.com>
2018-01-02 16:54:51 -0800

    Implement stack and queue classes

--- 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
+++ 01-data-structures/02-stacks-and-queues/myqueue/myqueue.rb
@@ -5,14 +5,21 @@ class MyQueue
   def initialize
     @queue = Array.new
     @head = @queue[0]
+    @tail = @queue[0]
   end
 
   def enqueue(element)
+    @queue << element
+    @head = @queue[0]
+    @tail = element
   end
 
   def dequeue
+    @queue.delete_at(0)
+    @head = @queue[0]
   end
 
   def empty?
+    @queue === [] ? true : false
   end
 end
