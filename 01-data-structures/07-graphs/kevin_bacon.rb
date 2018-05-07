7b71cc14e4a992b3dea70807c64c0ddebfa15fb9
Jason Li <jason.li0102@gmail.com>
2018-05-06 20:34:45 -0700

    Add Rspec tests for find_kevin_bacon method

--- 01-data-structures/07-graphs/kevin_bacon.rb
+++ 01-data-structures/07-graphs/kevin_bacon.rb
@@ -1,31 +1,40 @@
 require_relative "node"
 
-def find_kevin_bacon(start_node)
-  found = nil
-  current_path = []
-  current_path_actors = [start_node]
-  start_node.film_actor_hash.each do |film_name, list_of_actors|
-    current_path.include?(film_name) ? next : current_path.push[film_name]
+class KevinBacon
+  def initialize(start_node)
+    @current_path = []
+    @current_path_actors = [start_node.name]
+    @found = nil
+  end
 
-    list_of_actors.each do |actor|
-      next if current_path_actors.include?(actor)
-      current_path_actors.push(actor)
-      if actor.name === 'Kevin Bacon'
-        found = true
-      elsif current_path.length < 6
-        find_kevin_bacon(actor)
+  def find_kevin_bacon(start_node)
+    start_node.film_actor_hash.each do |film_name, list_of_actors|
+      @current_path.include?(film_name) ? next : @current_path.push(film_name)
+
+      list_of_actors.each do |actor|
+        next if @current_path_actors.include?(actor.name)
+        @current_path_actors.push(actor.name)
+        if actor.name === 'Kevin Bacon'
+          @found = true
+        elsif @current_path.length < 6
+          find_kevin_bacon(actor)
+        end
+        if @found
+          break
+        else
+          @current_path_actors.pop
+        end
       end
-      if found
+      if @found
         break
       else
-        current_path_actors.pop
+        @current_path.pop
       end
     end
-    if found
-      break
+    if @found
+      return @current_path
     else
-      current_path.pop
+      return 'Not Found'
     end
   end
-  return current_path
 end
