a1a5b0de70a41bb7bf2be9ea452b323805ef2b5e
Jason Li <jason.li0102@gmail.com>
2018-05-09 23:28:53 -0700

    Implement quick sort

---
+++ 02-algorithms-and-complexity/03-algorithms-sorting/quick_sort.rb
@@ -0,0 +1,23 @@
+def quick_sort(collection, first = 0,  last = collection.length - 1)
+  pivot = collection[last]
+  pivot_index = last
+  i = first
+  while i < pivot_index
+    if collection[i] > pivot
+      removed_element = collection.delete_at(i)
+      pivot_index -= 1
+      collection.insert((pivot_index + 1), removed_element)
+    else
+      i += 1
+    end
+  end
+  left = collection[first..(pivot_index - 1)]
+  right = collection[(pivot_index + 1)..last]
+  if left.length > 1
+    quick_sort(collection, first, pivot_index - 1)
+  end
+  if right.length > 1
+    quick_sort(collection, pivot_index + 1, last)
+  end
+  return collection
+end
